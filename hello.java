18423aa437ded74fc9f80f6582c32488e7cb861e
Pooja-Kale <95167758+Pooja-Kale@users.noreply.github.com>
2022-03-11 18:13:44 +0530

    addition java file

--- hello.java
+++ hello.java
@@ -1,8 +1,14 @@
-class hello
+// Simple hollo world program for java :
+
+//import java.util.scanner;
+
+public class hello
+
 {
-   public static void main(String [] args)
-   {
-   System.out.println(" Hello first java here...")
-   
-   }
+    public static void main(String args[])
+      {  
+         int a=100, b=20, sum=0;
+         sum = a + b;
+         System.out.println("Hollo Pooja Here ..." + sum);
+      }
 }
